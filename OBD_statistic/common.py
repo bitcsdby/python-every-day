cc0eee761d23330816fa691d97a4c0abd0ad114f
bitcsdby <bitcsdby@gmail.com>
2014-06-27 16:08:02 +0800

    add obd statistic

--- OBD_statistic/common.py
+++ OBD_statistic/common.py
@@ -5,14 +5,22 @@ __author__ = 'bitcsdby'
 class Rawdatastructure:
     def __init__(self, dataitem):
         ##define useful data domain here
-        self.mil_dist = 0.  # distance after malfunction indicator light on
-        self.clr_dist = 0.  # distance after diagnostic codes cleared
-        self.initial_mil = 0.  # total mil = initial_mil + mildist + clrdist
-        self.vss = 0.  # vehicle speed sensor
-        self.load_pct = 0.  # % load value of the engine
+        self.mil_dist = 0.00  # distance after malfunction indicator light on
+        self.clr_dist = 0.00  # distance after diagnostic codes cleared
+        self.initial_mil = 0.00  # total mil = initial_mil + mildist + clrdist
+        self.vss = 0.00  # vehicle speed sensor
+        self.load_pct = 0.00  # % load value of the engine
         self.rpm = 0  # round per minute of engine
-        self.app_r = 0  # % relative accelerator pedal position
+        self.app_r = 0.00  # % relative accelerator pedal position
         self.dataclean(dataitem)
+
+    def printvalues(self):
+        print 'MIL_DIST', self.mil_dist
+        print 'CLR_DIST', self.clr_dist
+        print 'VSS', self.vss
+        print 'LOAD_PCT', self.load_pct
+        print 'RPM', self.rpm
+        print 'APP_R', self.app_r
 
     """
     dataclean
@@ -20,38 +28,39 @@ class Rawdatastructure:
     """
     def dataclean(self, dataitem):
         # MIL_DIST km
+
         if dataitem['MIL_DIST'].isdigit():
-            self.mil_dist = dataitem['MIL_DIST']
+            self.mil_dist = float(dataitem['MIL_DIST'])
         else:
             print 'invalid MIL_DIST value', dataitem['MIL_DIST']
             return
         # CLR_DIST km
         if dataitem['CLR_DIST'].isdigit():
-            self.clr_dist = dataitem['CLR_DIST']
+            self.clr_dist = float(dataitem['CLR_DIST'])
         else:
             print 'invalid CLR_DIST value', dataitem['CLR_DIST']
 
         # VSS  km/H
         if dataitem['VSS'].isdigit():
-            self.vss = int(dataitem['VSS'])
+            self.vss = float(dataitem['VSS'])
         else:
             print 'invalid VSS_DIST value', dataitem['VSS']
 
         # LOAD_PCT  100 / 255 %
         if dataitem['LOAD_PCT'].isdigit():
-            self.load_pct = int(dataitem['LOAD_PCT']) / 255.0
+            self.load_pct = '%2.5f' % (float(dataitem['LOAD_PCT']) / 255.0 * 100)
         else:
             print 'invalid LOAD_PCT value', dataitem['LOAD_PCT']
 
         # RPM  1/4
         if dataitem['RPM'].isdigit():
-            self.rpm = int(dataitem['RPM']) * 0.25
+            self.rpm = int(dataitem['RPM']) / 4
         else:
             print 'invalid RPM value', dataitem['RPM']
 
         # APP_R 100 / 255%
         if dataitem['APP_R'].isdigit():
-            self.load_pct = int(dataitem['APP_R']) / 255.0
+            self.app_r = '%2.5f' % (float(dataitem['APP_R']) / 255.0 * 100)
         else:
             print 'invalid APP_R value', dataitem['APP_R']
 
--- OBD_statistic/statistic.py
+++ OBD_statistic/statistic.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
-__author__ = 'bitcsdby
+__author__ = 'bitcsdby'
 
 from common import Rawdatastructure
+import urllib2
+import json
+
 
 class Statistic:
     def __init__(self):
@@ -14,16 +17,45 @@ class Statistic:
         self.tripdistance = 0. ## total trip distance
         self.oilconsumption = 0. ## total energy consumption
         self.drivingscore = 0. ## driving score 
-        
-        
-    def getdatafromweb(self,start,end,url):
-        ##get data from a web sql. items from id start to id end
-        pass
 
-    def dataclean(self):
-        ##get useful items from every dbitem, convert raw data to data with real physical meanings
-        pass
+    def getdatafromweb(self, start, count, url):
+        ## get data from a web sql. items from id start to id end
+        url += 'from=' + str(start) + '&' + 'count=' + str(count)
+        print url
+        # from=113230&count=210
+
+        request = urllib2.Request(url)
+        html = urllib2.urlopen(request)
+
+        self.dbitems = json.loads(html.read())['msg']['data']
+
+        self.dbitems.reverse()
+
+        for item in self.dbitems:
+            if int(item['VSS']) == 0x88:
+                print 'invalid dbitem'
+                continue;
+            dsitem = Rawdatastructure(item)
+            self.dsitems.append(dsitem)
+
+        print len(self.dsitems)
+
+        # print self.dbitems
+        # print type(self.dbitems['data'])
+        # self.dbitems['data']
 
     def runstatistic(self):
-        ##statistic with dsitems
-        pass
+        print len(self.dsitems)
+        for item in self.dsitems:
+            item.printvalues()
+            print ''
+        ## statistic with dsitems
+        # print self.dbitems
+
+
+if __name__ == '__main__':
+    s = Statistic()
+    url = 'http://www.ecloudan.com/api.php?p=vanet.obd.getdata.obd&access_token=f313e38dfb990557b49f475d42e89237ddda905a34a086fa48e7f26d9894242b&'
+
+    s.getdatafromweb(113230, 210, url)
+    s.runstatistic()
