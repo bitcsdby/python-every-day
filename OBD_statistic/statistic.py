441a25f5ce43347ed399e500ce4961644fbb0311
bitcsdby <bitcsdby@gmail.com>
2014-06-30 22:35:58 +0800

    完成统计和评分

--- OBD_statistic/statistic.py
+++ OBD_statistic/statistic.py
@@ -87,6 +87,8 @@ class Statistic:
         maxspeed = 0.0      ## for maxspeed
         speedsum = 0.0      ## for average speed
         consumptionsum = 0.0        ## for averge oil consumption
+        minclr = 65535
+        minmil = 65535
 
         for i in range(l-1):
             self.dsitems[i].printvalues()
@@ -143,8 +145,9 @@ class Statistic:
         print '急踩油门次数', self.tostepongas
         print '高速巡航时间', self.tohighspeed / l * 100, '%'
         print '怠速时间', self.toidling / l * 100, '%'
-        print '行驶里程', '总里程', self.mildistance + self.clrdistance, 'Km', '  '\
-                          '故障', self.mildistance, '正常', self.clrdistance
+        print '行驶里程',  '总里程', self.mildistance + self.clrdistance - \
+                                    self.dsitems[0].mil_dist - self.dsitems[0].clr_dist, 'Km', '  '\
+                          '故障', self.mildistance - self.dsitems[0].mil_dist, '正常', self.clrdistance - self.dsitems[0].clr_dist
         print '平均速度', self.averagespeed, 'Km/h'
         print '平均油耗', self.oilconsumption, 'L/ 100km'
         print '驾驶评分', self.drivingscore
